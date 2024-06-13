f8c8eb4973cd15dbb3c4e43f64cfdda06e096c0f
Inès Richard <ines.richard@ntico.com>
2024-06-13 15:09:24 +0200

    creation de la page help, ajout des path dans le router et dans la barre de navigation

---
+++ src/pages/HelpPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="q-pa-lg q-tm-xs">
+    <h5 class="q-mt-none">Help</h5>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis,
+      doloremque. Maxime optio tempore iusto totam. Pariatur doloribus quibusdam
+      quam, reiciendis distinctio eaque illum mollitia dolorum, id animi
+      incidunt eligendi harum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis,
+      doloremque. Maxime optio tempore iusto totam. Pariatur doloribus quibusdam
+      quam, reiciendis distinctio eaque illum mollitia dolorum, id animi
+      incidunt eligendi harum.
+    </p>
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: "TodoPage",
+});
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,10 @@ const routes = [
   {
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("src/pages/TodoPage.vue") }],
+    children: [
+      { path: "", component: () => import("src/pages/TodoPage.vue") },
+      { path: "/help", component: () => import("src/pages/HelpPage.vue") },
+    ],
   },
 
   // Always leave this as last one,
