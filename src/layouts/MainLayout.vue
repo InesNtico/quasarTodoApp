e3d6fa7049b87669a4c4dfd997ba8c76d55ed378
Inès Richard <ines.richard@ntico.com>
2024-06-13 14:06:01 +0200

    dev du drawer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -18,20 +18,57 @@
       <q-img src="../statics/milky-way.jpg" class="header-image absolute-top" />
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header> Essential Links </q-item-label>
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      :width="200"
+      :breakpoint="600"
+    >
+      <q-scroll-area
+        style="
+          height: calc(100% - 192px);
+          margin-top: 192px;
+          border-right: 1px solid #ddd;
+        "
+      >
+        <q-list padding>
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="list" />
+            </q-item-section>
 
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+            <q-item-section> Todo </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="help" />
+            </q-item-section>
+
+            <q-item-section> Help </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+
+      <q-img
+        class="absolute-top bg-primary"
+        src="../statics/milky-way.jpg"
+        style="height: 192px"
+      >
+        <div class="absolute-bottom bg-transparent q-ml-md q-mb-md">
+          <q-avatar size="56px" class="q-mb-sm">
+            <img src="../statics/blank-profile-picture-973460_1280.png" />
+          </q-avatar>
+          <div class="text-weight-bold">toto</div>
+          <div>@toto</div>
+        </div>
+      </q-img>
     </q-drawer>
 
     <q-page-container>
-      <router-view />
+      <keep-alive>
+        <router-view />
+      </keep-alive>
     </q-page-container>
   </q-layout>
 </template>
